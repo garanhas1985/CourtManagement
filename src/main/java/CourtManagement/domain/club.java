d1bc93a3840c8aa0debbc45bf4ab5c54bfd39e3b
Nuno Cruz <1241919@isep.ipp.pt>
2025-07-09 17:13:15 +0100

    added new folders

---
+++ src/main/java/CourtManagement/domain/club.java
@@ -0,0 +1,4 @@
+package CourtManagement.domain;
+
+public class club {
+}
---
+++ src/test/java/CourtManagement/domain/clubTest.java
@@ -0,0 +1,7 @@
+package CourtManagement.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class clubTest {
+
+}
