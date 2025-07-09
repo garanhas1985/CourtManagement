b7c6814157000823a02c2f3831913e6d6316f46c
Nuno Cruz <1241919@isep.ipp.pt>
2025-07-09 17:14:48 +0100

    added spring repo

---
+++ src/main/java/CourtManagement/persistence/clubSpringRepo.java
@@ -0,0 +1,4 @@
+package CourtManagement.persistence;
+
+public class clubSpringRepo {
+}
---
+++ src/test/java/CourtManagement/persistence/clubSpringRepoTest.java
@@ -0,0 +1,7 @@
+package CourtManagement.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class clubSpringRepoTest {
+
+}
